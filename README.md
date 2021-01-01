d5008e679825641d49550a31e98b3cf7ce1fd962
Eric Seppanen <eds@reric.net>
2021-01-01 15:48:32 -0800

    improve documentation

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ This crate contains useful tools for working with the CBOR encoding format.
 It allows encoding arbitrary CBOR sequences, including:
 - indefinite-length encoding
 - non-canonical encoding of integers
+- tagged types
 - sequences that may fail in strict-mode decoders
 - malformed sequences (for testing decoders, perhaps)
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,58 @@
+//! `cbor-tools` is a library for manipulating CBOR-encoded data.
+//!
+//! **CBOR** is a data serialization format described in [RFC7049].
+//! CBOR is a binary-friendly self-describing data encoding that has
+//! built-in types for:
+//! - Integers and Floating point numbers
+//! - Arrays and Maps
+//! - Arbitrary-length UTF-8 text strings
+//! - Arbitrary-length bytestrings
+//!
+//! Other crates provide `serde` serialization and deserialization of
+//! native Rust data structurs.
+//!
+//! This crate provides tools for constructing CBOR with fine-grained
+//! control, including:
+//! - indefinite-length encoding
+//! - non-canonical encoding of integers
+//! - tagged types
+//! - sequences that may fail in strict-mode decoders
+//! - malformed sequences (for testing decoders, perhaps)
+//!
+//! To encode some data in CBOR, create one or more [`CborType`] values,
+//! and then call [`encode`] on them:
+//!
+//! ```
+//! use cbor_tools::{CborType, Encode};
+//!
+//! let my_data = vec![1, 2, 3];
+//! let cbor_tree = CborType::from(my_data);
+//! let cbor_bytes = cbor_tree.encode();
+//! // cbor_bytes is a Vec<u8>
+//! ```
+//!
+//! There is a `From<T>` implementation available for many simple types.
+//! Additional data structures can be built by hand, like this non-homogenous
+//! array:
+//!
+//! ```
+//! use cbor_tools::{CborType, Encode};
+//!
+//! // An array containing a string and an integer.
+//! let list = vec![
+//!     CborType::from("abc"),
+//!     CborType::from(123),
+//! ];
+//! let cbor_tree = CborType::from(list);
+//! let cbor_bytes = cbor_tree.encode();
+//! // cbor_bytes is a Vec<u8>
+//! # assert_eq!(cbor_bytes, [0x82, 0x63, 0x61, 0x62, 0x63, 0x18, 0x7b]);
+//! ```
+//!
+//! [RFC7049]: https://tools.ietf.org/html/rfc7049
+//! [`encode`]: Encode::encode
+//!
+
 use half::f16;
 use std::fmt;
 use std::{convert::TryFrom, ops::Deref};
